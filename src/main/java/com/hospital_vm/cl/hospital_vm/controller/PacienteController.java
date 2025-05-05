fd91d33d8bc832c51f403ade81b2da3ebc321d88
ProfeRicvrdo <ri.yaneza@profesor.duoc.cl>
2025-05-05 13:43:04 -0400

    "creacion proy cecto"

--- src/main/java/com/hospital_vm/cl/hospital_vm/controller/PacienteController.java
+++ src/main/java/com/hospital_vm/cl/hospital_vm/controller/PacienteController.java
@@ -1,6 +1,69 @@
 package com.hospital_vm.cl.hospital_vm.controller;
 
+import com.hospital_vm.cl.hospital_vm.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.hospital_vm.cl.hospital_vm.model.Paciente;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/pacientes")
 public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Paciente>> listar() {
+        List<Paciente> pacientes = pacienteService.findAll();
+        if (pacientes.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ResponseEntity.ok(pacientes);
+    }
+
+    @PostMapping
+    public ResponseEntity<Paciente> guardar(@RequestBody Paciente paciente) {
+        Paciente nuevoPaciente = pacienteService.save(paciente);
+        return new ResponseEntity<>(nuevoPaciente, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> buscar(@PathVariable long id) {
+        try{
+            Paciente paciente = pacienteService.findById(id);
+            return new ResponseEntity<>(paciente, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Paciente> actualizar(@PathVariable Long id, @RequestBody Paciente paciente) {
+        try {
+            Paciente pacienteModificado = pacienteService.findById(id);
+            pacienteModificado.setId(id.intValue());
+            pacienteModificado.setRun(paciente.getRun());
+            pacienteModificado.setNombres(paciente.getNombres());
+            pacienteModificado.setApellidos(paciente.getApellidos());
+            pacienteModificado.setFechaNacimiento(paciente.getFechaNacimiento());
+            pacienteModificado.setCorreo(paciente.getCorreo());
+
+            pacienteService.save(pacienteModificado);
+            return new ResponseEntity<>(pacienteModificado, HttpStatus.OK);
+
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+
+
+
 
 
 }
